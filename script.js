8f929838a01a0af3f6580276cc77c6f4ff0192a8
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-20 13:10:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let task=document.getElementById('timer');
+const date=new Date();
+ setInterval(function(){
+	date=new Date();
+	task.innerHTML=date.toLocaleString();
+},1000)
